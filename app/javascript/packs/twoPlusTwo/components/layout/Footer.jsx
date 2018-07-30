38be955a97df60ded08d43859de19a8c9e06b26a
Phil Brockwell <nineteen40eight@gmail.com>
2018-07-30 17:22:37 +0100

    Add social media links to Footer.jsx

--- app/javascript/packs/twoPlusTwo/components/layout/Footer.jsx
+++ app/javascript/packs/twoPlusTwo/components/layout/Footer.jsx
@@ -1,8 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+let socialMediaLinks = [
+	{
+		name: "youtube",
+		url: "https://www.youtube.com/channel/UCJVckPp2VNERm639Cj8-j7A"
+	},
+	{
+		name: "linkedin",
+		url: "https://uk.linkedin.com/in/phil-brockwell-18676087"
+	},
+	{ name: "github",
+		url: "https://github.com/phil-l-brockwell"
+	},
+	{ name: "twitter",
+		url: "https://twitter.com/pokerengineer"
+	}
+];
 
 class Footer extends React.Component {
 	render() {
-		return <p>This is the footer</p>;
+		return (
+			<footer>
+				{socialMediaLinks.map((link, i) => (
+					<Link to={link["url"]} key={i}>
+						<i className={`fa fa-${link["name"]} fa-lg`} aria-hidden="true" />
+					</Link>
+				))}
+			</footer>
+		);
 	}
 }
 
