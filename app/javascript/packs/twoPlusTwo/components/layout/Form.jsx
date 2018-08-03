87bb0aa297a7764fb625f984dc6614f16da7b394
Phil Brockwell <phil.l.brockwell@gmail.com>
2018-08-03 16:00:26 +0100

    Add generic Form component and replace PostForm and Login

---
+++ app/javascript/packs/twoPlusTwo/components/layout/Form.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import axios from "axios";
+import Input from "../layout/Input";
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fields: this.initFields(props.fields),
+      errorMessage: ""
+    };
+  }
+
+  render() {
+    return (
+      <div className="modal">
+        <form>
+          <i
+            className="fa fa-close"
+            aria-hidden="true"
+            onClick={this.props.toggle}
+          />
+          {this.props.fields.map((field, i) => (
+            <Input
+              key={i}
+              type={field.type}
+              name={field.name}
+              value={this.state.fields[field.name]}
+              onChange={e => this.updateField(e)}
+            />
+          ))}
+          <input
+            onClick={this.send.bind(this)}
+            type="submit"
+          />
+          <p>{this.state.errorMessage}</p>
+        </form>
+      </div>
+    );
+  }
+
+  send(e) {
+    e.preventDefault();
+    const that = this;
+
+    axios
+      .post(that.props.url, this.payload())
+      .then(function(response) {
+        const token = response.data["authenticity_token"];
+        if (token) {
+          that.props.updateCsrfToken(token);
+        }
+        that.props.callback();
+        that.clear();
+        that.props.toggle();
+      })
+      .catch(function(response) {
+        that.setState({ errorMessage: "Something went wrong!" });
+      });
+  }
+
+  initFields(fields) {
+    return fields.reduce((obj, item) => {
+      obj[item["name"]] = "";
+      return obj;
+    }, {});
+  }
+
+  clear() {
+    this.setState({
+      fields: this.initFields(this.props.fields),
+      errorMessage: ""
+    });
+  }
+
+  updateField(e) {
+    this.setState({
+      fields: {
+        ...this.state.fields,
+        [e.target.id]: e.target.value
+      }
+    });
+  }
+
+  payload() {
+    return {
+      [this.props.resource]: this.state.fields,
+      authenticity_token: this.props.authenticityToken
+    };
+  }
+}
+
+export default Form;
--- app/javascript/packs/twoPlusTwo/components/layout/Header.jsx
+++ app/javascript/packs/twoPlusTwo/components/layout/Header.jsx
@@ -2,10 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Avatar from "images/avatar.png";
 import SignOut from "../modals/SignOut";
-import LogIn from "../modals/LogIn";
-import PostForm from "../modals/PostForm";
+import Form from "./Form";
+
+const postFields = [{ name: "title" }, { name: "subtitle" }, { name: "text", type: "textarea" }];
+const userFields = [{ name: "email" }, { name: "password", type: "password" }];
 
 class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showPostForm: false,
+      showLoginForm: false
+    };
+  }
+
   render() {
     return (
       <header>
@@ -23,56 +33,51 @@ class Header extends React.Component {
           <Link to="/posts" className="button">
             blog
           </Link>
-          {this.userLinks()}
+          {this.props.currentUser ? (
+            <React.Fragment>
+              <button onClick={this.togglePostForm.bind(this)}>post</button>
+              <SignOut
+                csrfToken={this.props.csrfToken}
+                updateCsrfToken={this.props.updateCsrfToken}
+                getCurrentUser={this.props.getCurrentUser}
+              />
+            </React.Fragment>
+          ) : (
+            <button className="button" onClick={this.toggleLoginForm.bind(this)}>
+              log in
+            </button>
+          )}
         </div>
-        <LogIn
-          csrfToken={this.props.csrfToken}
-          updateCsrfToken={this.props.updateCsrfToken}
-          getCurrentUser={this.props.getCurrentUser}
-          ref={instance => {
-            this.logIn = instance;
-          }}
-        />
-        <PostForm
-          ref={instance => {
-            this.postForm = instance;
-          }}
-          fetchPosts={this.props.fetchPosts}
-        />
+        {this.state.showLoginForm ? (
+          <Form
+            authenticityToken={this.props.csrfToken}
+            updateCsrfToken={this.props.updateCsrfToken}
+            callback={this.props.getCurrentUser}
+            toggle={this.toggleLoginForm.bind(this)}
+            url="/users/sign_in.json"
+            resource="user"
+            fields={userFields}
+          />
+        ) : null}
+        {this.state.showPostForm ? (
+          <Form
+            callback={this.props.fetchPosts}
+            toggle={this.togglePostForm.bind(this)}
+            url="/api/posts"
+            resource="post"
+            fields={postFields}
+          />
+        ) : null}
       </header>
     );
   }
 
-  userLinks() {
-    if (this.props.currentUser) {
-      return (
-        <React.Fragment>
-          <button
-            onClick={() => {
-              this.postForm.show();
-            }}
-          >
-            post
-          </button>
-          <SignOut
-            csrfToken={this.props.csrfToken}
-            updateCsrfToken={this.props.updateCsrfToken}
-            getCurrentUser={this.props.getCurrentUser}
-          />
-        </React.Fragment>
-      );
-    } else {
-      return (
-        <button
-          className="button"
-          onClick={() => {
-            this.logIn.show();
-          }}
-        >
-          log in
-        </button>
-      );
-    }
+  togglePostForm() {
+    this.setState({ showPostForm: !this.state.showPostForm });
+  }
+
+  toggleLoginForm() {
+    this.setState({ showLoginForm: !this.state.showLoginForm });
   }
 }
 
