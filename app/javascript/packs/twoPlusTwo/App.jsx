d962103caf6d907492ec41c5cd237d7c77638af5
Phil Brockwell <phil.l.brockwell@gmail.com>
2018-08-21 11:58:49 +0100

    Add Layout.jsx

--- app/javascript/packs/twoPlusTwo/App.jsx
+++ app/javascript/packs/twoPlusTwo/App.jsx
@@ -1,22 +1,18 @@
 import React from "react";
-import axios from "axios";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Header from "./components/layout/Header";
-import Footer from "./components/layout/Footer";
 import PostsPage from "./components/pages/PostsPage";
 import { CurrentUserProvider } from "./CurrentUser";
+import Layout from "./components/layout/Layout";
 
 export default class App extends React.Component {
   render() {
     return (
       <CurrentUserProvider>
         <Router>
-          <div className="page">
-            <Header />
-            <Route exact path="/" component={PostsPage} />
-            <Route exact path="/posts" component={PostsPage} />
-            <Footer />
-          </div>
+          <Layout>
+            <Route path="/" component={PostsPage} />
+            <Route path="posts" component={PostsPage} />
+          </Layout>
         </Router>
       </CurrentUserProvider>
     );
---
+++ app/javascript/packs/twoPlusTwo/components/layout/Layout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default class Layout extends React.Component {
+  render() {
+    return (
+      <div className="page">
+        <Header />
+
+        {this.props.children}
+
+        <Footer />
+      </div>
+    );
+  }
+}
