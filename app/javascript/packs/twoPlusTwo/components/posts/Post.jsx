8b368b3262b1ca6f15dc0b8e5c36065902e09093
Phil Brockwell <phil.l.brockwell@gmail.com>
2018-08-25 22:36:20 +0100

    Display hero_image in Post.jsx and use default image

--- app/javascript/packs/twoPlusTwo/components/posts/Post.jsx
+++ app/javascript/packs/twoPlusTwo/components/posts/Post.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
+import DefaultImage from "images/banksy-hero.jpg";
 
 export default class Post extends React.Component {
   render() {
-    const { title, subtitle, text } = this.props.post;
+    const { title, subtitle, text, hero_image } = this.props.post;
+    const style = { backgroundImage: `url(${hero_image || DefaultImage})` };
 
     return (
       <div className="post">
-        <div className="background" />
+        <div className="background" style={style} />
         <div className="headings">
           <h1>{title}</h1>
           <h2>{subtitle}</h2>
